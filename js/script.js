96363606e8cb3f4d8470f178d6429cf2086191e6
bikajkula <mihajlo.karadzic@gmail.com>
2023-02-13 02:48:11 +0100

    Random number guesser
    Random number guesser

--- js/script.js
+++ js/script.js
@@ -1,42 +1,40 @@
-
-window.onkeyup = function(e) {
-   var key = e.keyCode ? e.keyCode : e.which;
-
-   if (key == 32) {
-       proveriUnos();
-   }
-}
-var i;
-var j;
-var vokabular="hocu necu mogu gde kako zasto koliko ja ti ona ono onaj oni one mi vi kako tako zato moram hteli dodji dole gore levo desno napred nazad takodje zajedno doci cu ce ne da sa zaliti kajati udarati smejati igrati";
-var reci = vokabular.split(" ");
-nasumiceReci=[20];
+var raspon=[5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];//nivoi
+var nasumiceNiz=[20];
 for(var i=0;i<20;i++){
-	j = Math.floor(Math.random() * (reci.length)); 
-	nasumiceReci[i]=reci[j];
+	nasumiceNiz[i] = Math.floor(Math.random() * (raspon[i]))+1; 
 }
-document.getElementById("trazeneReci").innerHTML=nasumiceReci;
-function proveriUnos(){
-	var rec=document.getElementById("unos").value;
-	document.getElementById("unos").value="";
-	rec=rec.split(" ");
-	var n=parseInt(document.getElementById("broj").innerHTML);
-	if(n==0){
-		document.getElementById('pocetak').innerHTML=Date.now();
+document.getElementById("btn-submit").addEventListener("mousedown",proveriBroj);
+function proveriBroj(){
+	var nivo=parseInt(document.getElementById("nivo").innerHTML)-1;//-1 jer se kupi kec
+	var bod=raspon[nivo];
+	var greske=parseInt(document.getElementById("greske").innerHTML);
+	var trenutnoBod=parseInt(document.getElementById("bodovi").innerHTML);
+	var tryBroj=parseInt(document.getElementById("tryBroj").value);
+	if(tryBroj>nasumiceNiz[nivo]){//ako je manje
+			document.getElementById("suggestionsBroj").innerHTML="Trazeni broj je manji od vaseg unetog";
+			document.getElementById('suggestionsBroj').style.color="red";
+			document.getElementById("greske").innerHTML=greske+1;
 	}
-		if(rec[0]===nasumiceReci[n]){
-			document.getElementById("rezultat").innerHTML="BRAVO";
-			document.getElementById("broj").innerHTML=n+1
-			if(n==19){
-				document.getElementById('kraj').innerHTML=Date.now();
-				var x=parseFloat(document.getElementById('pocetak').innerHTML);
-				var y=parseFloat(document.getElementById('kraj').innerHTML);
-				document.getElementById('vreme').innerHTML=(y-x)/1000 + " sekundi";
-				document.getElementById("rezultat").innerHTML="KRAJ";
-			}
+	else if(tryBroj<nasumiceNiz[nivo]){//ako je vece
+			document.getElementById("suggestionsBroj").innerHTML="Trazeni broj je veci od vaseg unetog";
+			document.getElementById('suggestionsBroj').style.color="blue";
+			document.getElementById("greske").innerHTML=greske+1;
+	}
+	else if(tryBroj!=nasumiceNiz[nivo]){
+		document.getElementById("suggestionsBroj").innerHTML="Pogresan unos";
+	}
+	else{//tacno
+		document.getElementById("suggestionsBroj").innerHTML="BRAVO";
+		document.getElementById('suggestionsBroj').style.color="green";
+		nivo++;
+		document.getElementById("bodovi").innerHTML=bod-greske+trenutnoBod;
+		document.getElementById("nivo").innerHTML=nivo+1;//vec je smanjeno na pocetku pa vracamo
+		document.getElementById("infoNivo").innerHTML="Broj se nalazi u rasponu [1-"+raspon[nivo]+"]";
+		document.getElementById("greske").innerHTML=0;//reset greske
+		if(nivo>19){
+			document.getElementById("igra").style.display="none";
+			document.getElementById("finalPic").style.display="block";
+			document.getElementById("final").innerHTML="OSVOJILI STE UKUPNO "+(bod-greske+trenutnoBod)+" BODA!";
 		}
-		else{
-			document.getElementById("rezultat").innerHTML="NETACNO! POKUSAJ PONOVO!";
-		}
-	
+	}
 }
